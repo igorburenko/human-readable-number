f71d545c06c61b9ef0d914fbe68d3d08afb28a66
Ihor Burenko <burenko@gmail.com>
2020-02-19 22:49:25 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let numbersWordsArr = [['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen'],
+        ['twen', 'thir', 'for', 'fif', 'six', 'seven', 'eigh', 'nine']];
+
+    if (number < 15) {
+        return numbersWordsArr[0][number]
+    } else if (number < 20) {
+        return numbersWordsArr[1][Math.floor(number - 10) - 2] + 'teen';
+    } else if (number < 100) {
+        let str = numbersWordsArr[1][Math.floor(number / 10) - 2] + 'ty';
+        return !(number % 10) ? str : str + " " + numbersWordsArr[0][number % 10]
+    } else {
+        let str = numbersWordsArr[0][Math.floor(number / 100)] + ' ' + 'hundred'
+        return !(number % 100) ? str :
+            str + " " + toReadable(number % 100)
+    }
+};
+
